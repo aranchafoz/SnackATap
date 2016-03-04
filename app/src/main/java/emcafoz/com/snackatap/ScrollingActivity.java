e89aef38978bbb3d770ddab30c6818ffa0796fcb
enriquemc96 <enriquemc96@gmail.com>
2016-03-04 10:14:24 +0100

    Added map to the activity of the product

--- app/src/main/java/emcafoz/com/snackatap/ScrollingActivity.java
+++ app/src/main/java/emcafoz/com/snackatap/ScrollingActivity.java
@@ -1,24 +1,46 @@
 package emcafoz.com.snackatap;
 
+import android.graphics.Point;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import emcafoz.com.snackatap.modelos.Edificio;
+import emcafoz.com.snackatap.modelos.Producto;
+import emcafoz.com.snackatap.sqlite.MySQLiteHelper;
+
 public class ScrollingActivity extends AppCompatActivity {
+
+    public GoogleMap googleMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
+        googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        googleMap.getUiSettings().setZoomGesturesEnabled(false);
 
+        googleMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                LatLng focus = new LatLng(location.getLatitude(), location.getLongitude());
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(focus, 15));
+            }
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -27,11 +49,30 @@ public class ScrollingActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getBundleExtra("producto");
 
+        MySQLiteHelper helper = new MySQLiteHelper(this);
+        Producto producto = helper.getProducto(bundle.getString("nombre"));
+        for (Edificio edificio : producto.getEdificios()) {
+            googleMap.addMarker(new MarkerOptions().position(new LatLng(
+                    edificio.getCx(), edificio.getCy())).title(edificio.getNombre()));
+        }
+
+        googleMap.setMyLocationEnabled(true);
+
+        LatLng focus;
+        try {
+            Location location = googleMap.getMyLocation();
+            focus = new LatLng(location.getLatitude(), location.getLongitude());
+
+        } catch (Exception e) {
+            focus = new LatLng(producto.getEdificios().get(0).getCx(), producto.getEdificios().get(0).getCy());
+        }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(focus, 15));
+
         TextView nombreProducto = (TextView) findViewById(R.id.nombre_producto);
-        nombreProducto.setText(bundle.getString("nombre"));
+        nombreProducto.setText(producto.getNombre());
 
         nombreProducto = (TextView) findViewById(R.id.precio_producto);
-        nombreProducto.setText(bundle.getFloat("precio") + " €");
+        nombreProducto.setText(String.format("%.2f", producto.getPrecio()) + "€");
 
         //endregion
 
@@ -42,8 +83,8 @@ public class ScrollingActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 //region reemplazar con Google Maps
-               // Snackbar.make(view, getResources().getString(R.string.snackbar), Snackbar.LENGTH_LONG)
-               //         .setAction("Action", null).show();
+                // Snackbar.make(view, getResources().getString(R.string.snackbar), Snackbar.LENGTH_LONG)
+                //         .setAction("Action", null).show();
                 //endregion
 
             }
